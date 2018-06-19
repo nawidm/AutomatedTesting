d213f4232c920e23ddc2a7713b50fc2c149dcc70
nawidm <nawid_m@hotmail.co.uk>
2018-06-19 12:02:21 +0100

    demoQA with report

--- demoQA/src/test/java/com/qa/quickstart/demoQA/demoQATest.java
+++ demoQA/src/test/java/com/qa/quickstart/demoQA/demoQATest.java
@@ -11,13 +11,18 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class demoQATest {
 
 	ChromeDriver driver;
+	static ExtentReports extent;
 	@BeforeClass
 	public static void initialise() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Documents\\chromedriver.exe");
-		
+		extent = new ExtentReports("C:\\Users\\Admin\\Documents\\AutomatedTesting\\demoQA\\extent.html", true);
 	}
 	
 	@Before
@@ -30,18 +35,33 @@ public class demoQATest {
 	
 	@Test
 	public void testDroppable() {
-
+		ExtentTest test = extent.startTest("Testing Droppable");
+		
 		driver.findElement(By.linkText("Droppable")).click();
+		test.log(LogStatus.INFO, "Droppable page loaded");
 		WebElement From=driver.findElement(By.id("draggableview"));
+		test.log(LogStatus.INFO, "Element to be dragged identified");
 		WebElement To=driver.findElement(By.id("droppableview"));
+		test.log(LogStatus.INFO, "Element to be dragged to identified");
 		Actions act = new Actions(driver);
 		act.dragAndDrop(From, To).build().perform();
-	    assertTrue(driver.findElement(By.id("droppableview")).getText().matches("Dropped!"));
+		test.log(LogStatus.INFO, "Element dragged");
+		try {
+			assertTrue(driver.findElement(By.id("droppableview")).getText().matches("Dropped!"));
+			test.log(LogStatus.PASS, "Drag and drop successfully registered");
+		} catch (AssertionError e) {
+			test.log(LogStatus.FAIL, "Drag and drop was not successful");
+			fail();
+		} finally {
+			test.log(LogStatus.INFO, "Current URL: " + driver.getCurrentUrl());
+			extent.endTest(test);
+		}
 	}
 	
 	
 	@After
 	public void tearDown() {
 		driver.close();
+		extent.flush();
 	}
 }
